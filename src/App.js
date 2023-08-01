fd0d540ac7a24b6d885057c203cfaddf9e9505c3
Adesewa Aishat <adesewaaishat@gmail.com>
2023-08-01 18:07:37 +0100

    fix route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import './App.css';
 import BestBook from './components/BestBook';
-import {Route,BrowserRouter,Routes} from 'react-router-dom'
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
-            <div>
-                <Routes>
-                    <Route path="/About" element={<About/>} />
-                    <Route path="/BestBook" element={<BestBook/>} />
-                </Routes>
-            </div>
-        </BrowserRouter>
+        <Routes>
+          <Route path='/' element={<BestBook />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+       </BrowserRouter>
     </div>
   );
 }
