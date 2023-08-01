9ab0ae0258827e77dc96720c190aa83c77353c7d
raaqiya ali <raaqiyaali_@hotmail.co.uk>
2023-08-01 16:01:42 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
 import './App.css';
-import BestBooks from './components/BestBook';
+import BestBook from './components/BestBook';
+import {Route,BrowserRouter,Routes} from 'react-router-dom'
+import About from './components/about';
 
 function App() {
   return (
     <div className="App">
-      <BestBooks />
+      <BrowserRouter>
+            <div>
+                <Routes>
+                    <Route path="/About" element={<About/>} />
+                    <Route path="/BestBook" element={<BestBook/>} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     </div>
   );
 }
