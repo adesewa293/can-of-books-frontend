d94e82b3a8ad2bd7747bf0424d204a98d0983510
Adesewa Aishat <adesewaaishat@gmail.com>
2023-08-01 18:08:51 +0100

    fix helmet error

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import './App.css';
+import { HelmetProvider } from 'react-helmet-async';
 import BestBook from './components/BestBook';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import About from './components/About';
+import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<BestBook />} />
-          <Route path='/about' element={<About />} />
-        </Routes>
-       </BrowserRouter>
+      <HelmetProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<BestBook />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      </HelmetProvider>
     </div>
   );
 }
