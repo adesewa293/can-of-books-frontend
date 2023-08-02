ca3fb09eeb8e2e8799a7c8d12115d8811da92404
Adesewa Aishat <adesewaaishat@gmail.com>
2023-08-02 09:44:43 +0100

    fix modal onclose

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,24 +1,20 @@
-import {React, useState} from 'react'
-import BookFormModal from './modal'
+import { React, useState } from "react";
+import BookFormModal from "./modal";
 
 export default function AddBook() {
-  const [isOpen, setIsOpen] = useState(false)
-  
+  const [isOpen, setIsOpen] = useState(false);
+
   function handleClose() {
+    setIsOpen(false);
+  }
+  function handleClick() {
     setIsOpen(true);
   }
-  function handleClick(){
-    console.log('before', isOpen)
-    setIsOpen(true);
-    console.log('after', isOpen)
-  }
-  
-  console.log('inside', isOpen)
+
   return (
     <div>
       <button onClick={handleClick}>AddBook</button>
-      <BookFormModal show={isOpen} onClose={handleClose}/>
+      <BookFormModal show={isOpen} onClose={handleClose} />
     </div>
-    
-  )
+  );
 }
