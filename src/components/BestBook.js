d57d07018fd24b7a83bfdea498a3d7bff777ee65
Adesewa Aishat <adesewaaishat@gmail.com>
2023-08-02 12:19:58 +0100

    add delete message

--- src/components/BestBook.js
+++ src/components/BestBook.js
@@ -5,30 +5,34 @@ import "./Modal.css";
 
 export default function BestBook() {
   const [books, setBooks] = useState([]);
+  const [message, setMessage] = useState(null);
 
   const getBooks = async () => {
     const response = await axios.get("https://canob.onrender.com/books/");
     setBooks(response.data);
   };
- async function handleDelete(id){
-  await axios.delete(`https://canob.onrender.com/books/${id}`);
- }
+
+  async function handleDelete(id) {
+    const response = await axios.delete(`https://canob.onrender.com/books/${id}`);
+    setMessage(response.data.message);
+    getBooks();
+  }
+
   useEffect(() => {
     getBooks();
   }, []);
 
-getBooks();
-
   return (
     <div>
       <h1>Best books</h1>
+      <i>{message}</i>
       {books.length > 0 ? (
         books.map((book) => (
           <div key={book._id}>
             <b>{book.title}</b>
             <p>{book.description}</p>
             <p>{book.status}</p>
-            <button onClick={()=> handleDelete(book._id)}>Delete Book</button>
+            <button onClick={() => handleDelete(book._id)}>Delete Book</button>
           </div>
         ))
       ) : (
