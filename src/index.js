ed67efa5bb28b6fcfc9bbe6a659ae1dcf8fc3db5
raaqiya ali <raaqiyaali_@hotmail.co.uk>
2023-08-03 20:16:38 +0100

    update

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <h2>Hello, from Index</h2>
       <App />
     </HelmetProvider>
   </React.StrictMode>
