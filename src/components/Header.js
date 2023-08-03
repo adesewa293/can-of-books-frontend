007671739dc1ead46986795ce2b5ed64231c9b3a
raaqiya ali <raaqiyaali_@hotmail.co.uk>
2023-08-03 16:43:13 +0100

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,8 @@ function Header() {
         <nav>
             <NavLink activeClassName="active" to="/BestBook">BestBook</NavLink>
             <NavLink  activeClassName="active" to="/About">About Us</NavLink>
-            <button ClassName="btn">Log in</button><button>Log out</button>
+            <button ClassName="btn">Log in</button>
+            <button>Log out</button>
         </nav>
 
         );
